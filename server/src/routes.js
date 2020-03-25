e635f4958dabee3a54756298bce2e0974159ce9e
Diogo Paes <diogopaes20@gmail.com>
2020-03-25 01:15:43 -0300

    Created controllers/routes for ongs and incidents

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+
 const routes = express.Router();
 
-routes.post('/user', (req, res) => {
-  const body = req.body;
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-  return res.json(body);
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
